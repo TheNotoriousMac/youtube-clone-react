89f92e81073b6818b40cb21cee7a73516b21bace
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-11-01 18:01:48 -0400

    Finshed Navbar, Started working on Sidebar.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Avatar, WhiteLogo } from "../../assets/ImageIndex";
 
 const Navbar = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [openNav, setOpenNav] = useState(false);
+  const [search, setSearch] = useState();
   return (
     <>
       <header>
@@ -19,8 +22,20 @@ const Navbar = () => {
           </div>
           <div className="searchContainer">
             <div className="searchBox">
-              <input type="text" placeholder="Search" />
-              <span className="material-symbols-rounded">close</span>
+              <input
+                type="text"
+                placeholder="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              {search !== "" && (
+                <span
+                  className="material-symbols-rounded"
+                  onClick={() => setSearch("")}
+                >
+                  close
+                </span>
+              )}
             </div>
             <div className="searchBtn">
               <span className="material-symbols-rounded">search</span>
@@ -32,17 +47,22 @@ const Navbar = () => {
           <div className="profileContainer">
             <span className="material-symbols-rounded">add_box</span>
             <span className="material-symbols-rounded">notifications</span>
-            <img src={Avatar} alt="" title="TheNotoriousMac" />
+            <img
+              src={Avatar}
+              alt=""
+              title="TheNotoriousMac"
+              onClick={() => setOpenNav(!openNav)}
+            />
           </div>
         </div>
 
-        <div className="profileBtns">
-          <div className="avatarInfo profileTab">
-            <img src={Avatar} alt="Your Avatar" title="Avatar" />
-            <p>TheNotoriousMac</p>
-          </div>
-          <div className="horizontalLine"></div>
-          <div className="profileTabs">
+        {openNav && (
+          <div className="profileBtns">
+            <div className="avatarInfo profileTab">
+              <img src={Avatar} alt="Your Avatar" title="Avatar" />
+              <p>TheNotoriousMac</p>
+            </div>
+            <div className="horizontalLine"></div>
             <div className="profileTabs">
               <div className="profileTab">
                 <span className="material-symbols-rounded">account_box</span>
@@ -52,9 +72,58 @@ const Navbar = () => {
                 <span className="material-symbols-rounded">play_circle</span>
                 <p>Youtube Studio</p>
               </div>
+              <div className="profileTab">
+                <span className="material-symbols-rounded">logout</span>
+                <p>Sign Out</p>
+              </div>
+            </div>
+            <div className="horizontalLine"></div>
+            <div className="profileTabs">
+              <div className="profileTab">
+                <span className="material-symbols-rounded">
+                  monetization_on
+                </span>
+                <p>Purchase and Memberships</p>
+              </div>
+              <div className="profileTab">
+                <span className="material-symbols-rounded">
+                  admin_panel_settings
+                </span>
+                <p>Your Data on Youtube</p>
+              </div>
+            </div>
+            <div className="horizontalLine"></div>
+            <div className="profileTabs">
+              {isDarkTheme && (
+                <div className="profileTab">
+                  <span className="material-symbols-rounded">light_mode</span>
+                  <p>Light Theme</p>
+                </div>
+              )}
+              {isDarkTheme && (
+                <div className="profileTab">
+                  <span className="material-symbols-rounded">bedtime</span>
+                  <p>Dark Theme</p>
+                </div>
+              )}
+              <div className="profileTab">
+                <span className="material-symbols-rounded">settings</span>
+                <p>Settings</p>
+              </div>
+            </div>
+            <div className="horizontalLine"></div>
+            <div className="profileTabs">
+              <div className="profileTab">
+                <span className="material-symbols-rounded">help</span>
+                <p>Help</p>
+              </div>
+              <div className="profileTab">
+                <span className="material-symbols-rounded">sms_failed</span>
+                <p>Send Feedback</p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </header>
     </>
   );
